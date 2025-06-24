c6bbdaec445d6359a5a10ab315fd3fd12ae98d39
Andrej Schwanke <as@software-design.de>
2025-06-24 09:29:47 +0200

    Re export api and client as monorepos are not properly installed when installed with git.

---
+++ index.js
@@ -0,0 +1,4 @@
+// Re-export the SDK from the root for GitHub installs
+export * from './packages/ugc_guard_sdk/src/index.js';
+// Re-export the OpenAPI client from the root for GitHub installs
+export * from './packages/api/dist/index.js';
