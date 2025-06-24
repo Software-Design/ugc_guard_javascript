392433e814d206a21f81053aa12db5b1f641e252
Andrej Schwanke <as@software-design.de>
2025-06-24 09:42:24 +0200

    Fix export

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Re-export the SDK from the root for GitHub installs
 export * from './packages/ugc_guard_sdk/src/index.js';
 // Re-export the OpenAPI client from the root for GitHub installs
-export * from './packages/api/dist/index.js';
+import * as UGCGuardApi from './packages/api/dist/index.js';
+export { UGCGuardApi };
